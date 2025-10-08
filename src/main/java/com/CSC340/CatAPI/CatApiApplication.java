a4592404620df9799ae1a09c2dd569b09e680e26
Adrian Aldridge <adriangaldridge@gmail.com>
2025-10-08 13:27:09 -0400

    Added files from spring initializer

---
+++ src/main/java/com/CSC340/CatAPI/CatApiApplication.java
@@ -0,0 +1,13 @@
+package com.CSC340.CatAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatApiApplication.class, args);
+	}
+
+}
